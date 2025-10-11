5e89fe3f062eca495101c99b9b95c356ed209314
Luke Janssen <jukelanssen64@gmail.com>
2025-10-11 07:43:16 -0700

    Finished Tailwind styling on core components

---
+++ section5/tailwind-demo/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // Flattened structure - much cleaner
+  const animalInfo = [
+    {
+      id: 1,
+      name: "dog",
+      sound: "Woof",
+      color: "Brown",
+      size: "Medium",
+    },
+    {
+      id: 2,
+      name: "cat",
+      sound: "Meow",
+      color: "Black",
+      size: "Small",
+    },
+    {
+      id: 3,
+      name: "cow",
+      sound: "Moo",
+      color: "White and Black",
+      size: "Large",
+    },
+  ];
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="mt-10 text-lg">
+        This is a simple React application styled with Tailwind CSS.
+      </p>
+      <div className="m-1 p-2 border border-gray-300 rounded-lg bg-gray-50">
+        {animalInfo.map((animal) => (
+          <div key={animal.id} className="mb-8">
+            <h2 className="text-2xl font-semibold capitalize">{animal.name}</h2>
+            <p>Sound: {animal.sound}</p>
+            <p>Color: {animal.color}</p>
+            <p>Size: {animal.size}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
