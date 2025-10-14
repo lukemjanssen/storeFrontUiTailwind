a6aff308a89ddacc2aa8f3ed2cf4ff062bc9aa4a
Luke Janssen <admin@Lukes-MacBook-Pro.local>
2025-10-14 10:29:44 -0700

    minor tweaks

--- section5/tailwind-demo/src/App.jsx
+++ section5/tailwind-demo/src/App.jsx
@@ -33,16 +33,16 @@ function App() {
       <p className="mt-10 text-lg">
         This is a simple React application styled with Tailwind CSS.
       </p>
-      <div className="m-1 p-2 border border-gray-300 rounded-lg bg-gray-50">
+      <div className="mb-8 grid grid-cols-4 gap-2 border m-8 p-8 justify-center border-gray-300 rounded-lg bg-gray-50">
         {animalInfo.map((animal) => (
-          <div key={animal.id} className="mb-8 grid grid-cols-4 gap-2 p-4 border-b border-gray-200 last:border-0">
+          <div key={animal.id} className="p-4 border rounded-lg shadow-2xl border-gray-500 last:border-0">
             <h2 className="text-2xl font-semibold capitalize">{animal.name}</h2>
             <p>Sound: {animal.sound}</p>
             <p>Color: {animal.color}</p>
             <p>Size: {animal.size}</p>
           </div>
         ))}
-        <button className="m-2 p-2 border border-gray-300 rounded-lg bg-gray-50 hover:bg-gray-700 ease-in-out transform duration-500 hover:scale-110"><h1 className="">Add Animal</h1></button>
+        <button className="m-2 p-2 border border-gray-300 rounded-lg bg-gray-50 hover:bg-gray-700 hover:text-white ease-in-out transform duration-500 hover:scale-110"><h1 className="">Add Animal</h1></button>
       </div>
     </>
   );
